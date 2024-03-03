10706b9f02c1cab825593d9657107fe4813ca318
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2024-03-03 12:29:32 +0700

    remove file

--- Basic Program/Vehicle.java
+++
@@ -1,6 +0,0 @@
-class Vehicle {
-    protected String brand = "Ford";        // Vehicle attribute
-    public void honk() {                    // Vehicle method
-      System.out.println("Tuut, tuut!");
-    }
-  }
