61bbe1bd12b6b48bb69bab2c329ebe8ff0524a85
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2024-03-06 17:10:34 +0700

    4 cach ep kieu trong java

---
+++ Basic Program/epkieu/vidu.java
@@ -0,0 +1,30 @@
+package epkieu;
+
+public class vidu {
+    public static void main(String[] args) {
+        int a = 100; 
+        int b = 2; 
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // ep kieu ngam dinh
+        float c =  a; 
+
+        System.out.println("c = " + c);
+
+        // ep kieu tuong minh 
+        float e = 3.5f; 
+        int g = (int) e; 
+
+        System.out.println("e = " + e);
+        System.out.println("g = " + g);
+
+        // ep kieu du lieu doi tuong: cac class chuyen doi phai ke thua nhau.
+        // (new class)object;
+
+        //Ep kieu giua bien nguyen thuy va doi tuong.
+        int x = new Integer(32);
+        System.out.println("x = " + x);
+    }
+}
