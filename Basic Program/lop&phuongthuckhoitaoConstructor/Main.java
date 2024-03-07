43ab958009a22864eefd7c0871d9fa0325825559
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2024-03-07 07:30:16 +0700

    Phương thức constructor - phuong thức mặc địng

---
+++ Basic Program/lop&phuongthuckhoitaoConstructor/Main.java
@@ -0,0 +1,10 @@
+
+class Main {
+    public static void main(String[] args) {
+        MyDate md = new MyDate(3,7,2024);
+        md.printDay(); 
+        md.printMonth();
+        md.printYear();
+        md.printDate();
+    }
+}
