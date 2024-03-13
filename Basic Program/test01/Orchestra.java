f8f0628d82e63ec4bbe68d6c99190961d6160ac2
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2024-03-13 17:18:39 +0700

    code mau

---
+++ Basic Program/test01/Orchestra.java
@@ -0,0 +1,49 @@
+public class Orchestra {
+
+    private List<Instrument> instruments;
+
+    public Orchestra() {
+        instruments = new ArrayList<>();
+    }
+
+    public void addInstrument(Instrument instrument) {
+        instruments.add(instrument);
+    }
+
+    public void playAll() {
+        for (Instrument instrument : instruments) {
+            instrument.play();
+        }
+    }
+
+    public void tuneAll() {
+        for (Instrument instrument : instruments) {
+            if (instrument instanceof Wind) {
+                ((Wind) instrument).play(Note.MIDDLE_C);
+            } else if (instrument instanceof Percussion) {
+                ((Percussion) instrument).adjust("vặn nút");
+            } else if (instrument instanceof Stringed) {
+                ((Stringed) instrument).adjust(2);
+            }
+        }
+    }
+
+    public void adjustWind() {
+        for (Instrument instrument : instruments) {
+            if (instrument instanceof Wind) {
+                if (instrument instanceof Brass) {
+                    ((Brass) instrument).adjust("nút ngậm");
+                } else if (instrument instanceof Woodwind) {
+                    ((Woodwind) instrument).adjust("nút thổi");
+                }
+            }
+        }
+    }
+
+    public void printAll() {
+        for (Instrument instrument : instruments) {
+            System.out.println(instrument.what);
+        }
+    }
+}
+
