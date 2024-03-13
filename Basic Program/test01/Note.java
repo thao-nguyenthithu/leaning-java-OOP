140a4b9af456c1ab64f1b592550738bd1c6fcfd1
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2024-03-13 17:32:37 +0700

    add class

---
+++ Basic Program/test01/Note.java
@@ -0,0 +1,78 @@
+
+public class Note {
+    private String A;
+    private String B;
+    private String Middle_C;
+    private String D;
+    private String E;
+    private String F;
+    private String G;
+
+    public Note(String a, String b, String middle_c, String d, String e, String f, String g) {
+        A = a;
+        B = b;
+        Middle_C = middle_c;
+        D = d;
+        E = e;
+        F = f;
+        G = g;
+    }
+
+    public String getA() {
+        return A;
+    }
+
+    public void setA(String a) {
+        A = a;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String b) {
+        B = b;
+    }
+
+    public String getMiddle_C() {
+        return Middle_C;
+    }
+
+    public void setMiddle_C(String middle_C) {
+        Middle_C = middle_C;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String d) {
+        D = d;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public void setE(String e) {
+        E = e;
+    }
+
+    public String getF() {
+        return F;
+    }
+
+    public void setF(String f) {
+        F = f;
+    }
+
+    public String getG() {
+        return G;
+    }
+
+    public void setG(String g) {
+        G = g;
+    }
+
+    
+}
