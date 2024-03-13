01f51be80ff6a9fb8abe0d87bfd80f69db8fe391
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2024-03-13 17:27:15 +0700

    add wind class

---
+++ Basic Program/test01/Wind.java
@@ -0,0 +1,11 @@
+public abstract class Wind implements Instrument {
+    @Override
+    public abstract void play();
+
+    @Override
+    public abstract String what();
+
+    @Override
+    public abstract void adjust();
+}
+
